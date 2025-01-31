d44eef5e055ee415dd689119f0a1e2e11c796df0
Areeba Web Developer <tariqareeba635@gmail.com>
2025-01-31 13:26:36 +0500

    hello.c
    nothing much

---
+++ hello.c
@@ -0,0 +1,12 @@
+#include <iostream> // Include the input-output stream library
+
+
+
+int main()
+{
+    std::cout << "Hello, World!" << std::endl; // Output "Hello, World!" to the console
+    return 0;                                  // Return 0 indicating successful program termination
+}
+
+
+
